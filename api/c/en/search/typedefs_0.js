a1cfa33fdbdbd5e0eea0eee7a9d6d499507395d3
Kazuki Kyakuno <kyakuno@axell.co.jp>
2024-04-23 16:33:00 +0900

    ailia TFLite Runtime 1.1.13

--- api/c/en/search/typedefs_0.js
+++ api/c/en/search/typedefs_0.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['ailiatflitecpufeatures_132',['AILIATFLiteCpuFeatures',['../ailia__tflite_8h.html#af403c38c72b52dcbceb46e62fc8f4561',1,'ailia_tflite.h']]],
-  ['ailiatfliteenvironment_133',['AILIATFLiteEnvironment',['../ailia__tflite_8h.html#ad3efb8abac41021597407f121eed535d',1,'ailia_tflite.h']]],
-  ['ailiatfliteflags_134',['AILIATFLiteFlags',['../ailia__tflite_8h.html#a102124bd44711479870d1515284d1a19',1,'ailia_tflite.h']]],
-  ['ailiatflitememorymode_135',['AILIATFLiteMemoryMode',['../ailia__tflite_8h.html#a9c44ccf3ba49f821be2629bd3dcde53c',1,'ailia_tflite.h']]],
-  ['ailiatflitestatus_136',['AILIATFLiteStatus',['../ailia__tflite_8h.html#afda2f6d89489d73ae1b983b2a8903cf2',1,'ailia_tflite.h']]],
-  ['ailiatflitetensortype_137',['AILIATFLiteTensorType',['../ailia__tflite_8h.html#ad484d38dd88bf46de076f88cc2bff74c',1,'ailia_tflite.h']]]
+  ['ailiatflitecpufeatures_134',['AILIATFLiteCpuFeatures',['../ailia__tflite_8h.html#af403c38c72b52dcbceb46e62fc8f4561',1,'ailia_tflite.h']]],
+  ['ailiatfliteenvironment_135',['AILIATFLiteEnvironment',['../ailia__tflite_8h.html#ad3efb8abac41021597407f121eed535d',1,'ailia_tflite.h']]],
+  ['ailiatfliteflags_136',['AILIATFLiteFlags',['../ailia__tflite_8h.html#a102124bd44711479870d1515284d1a19',1,'ailia_tflite.h']]],
+  ['ailiatflitememorymode_137',['AILIATFLiteMemoryMode',['../ailia__tflite_8h.html#a9c44ccf3ba49f821be2629bd3dcde53c',1,'ailia_tflite.h']]],
+  ['ailiatflitestatus_138',['AILIATFLiteStatus',['../ailia__tflite_8h.html#afda2f6d89489d73ae1b983b2a8903cf2',1,'ailia_tflite.h']]],
+  ['ailiatflitetensortype_139',['AILIATFLiteTensorType',['../ailia__tflite_8h.html#ad484d38dd88bf46de076f88cc2bff74c',1,'ailia_tflite.h']]]
 ];
