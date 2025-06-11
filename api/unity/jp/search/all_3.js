aa6613dd27299573398a12dbae2df1e2f04292ae
Kazuki Kyakuno <kyakuno@axell.co.jp>
2025-06-11 16:59:00 +0900

    Release ailia TFLite Runtime 1.22

--- api/unity/jp/search/all_3.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['getinputtensorinfo_92',['GetInputTensorInfo',['../classAiliaTFLiteModel.html#afaf7ac550d327294ad089097b4317d83',1,'AiliaTFLiteModel']]],
-  ['getinputtensorshape_93',['GetInputTensorShape',['../classAiliaTFLiteModel.html#a40b1bb006a8e8d1e89a13db08bfcf05e',1,'AiliaTFLiteModel']]],
-  ['getoutputtensordata_94',['GetOutputTensorData',['../classAiliaTFLiteModel.html#a4ae2a8d344ea6f853610de7b07860c26',1,'AiliaTFLiteModel']]],
-  ['getoutputtensorinfo_95',['GetOutputTensorInfo',['../classAiliaTFLiteModel.html#a360a88b6ee49e42329c6ce4b2683f7b0',1,'AiliaTFLiteModel']]],
-  ['getoutputtensorshape_96',['GetOutputTensorShape',['../classAiliaTFLiteModel.html#a8befb0b6c3df7301b3f67ce2395c59f1',1,'AiliaTFLiteModel']]],
-  ['getsummary_97',['GetSummary',['../classAiliaTFLiteModel.html#a1149c8481fcaa1da63da541b4784b4a1',1,'AiliaTFLiteModel']]]
-];
